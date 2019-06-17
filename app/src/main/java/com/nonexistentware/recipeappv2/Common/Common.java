c0eb76fffcc8da5d427b5a28391c7957e333ad57
IgorSinchuk <igorsinchuk@mail.ru>
2019-06-17 21:59:04 +0300

    Fragment useful works

--- app/src/main/java/com/nonexistentware/recipeappv2/Common/Common.java
+++ app/src/main/java/com/nonexistentware/recipeappv2/Common/Common.java
@@ -5,6 +5,7 @@ import com.nonexistentware.recipeappv2.Model.RecipeItem;
 public class Common {
     public static final String STR_CATEGORY_BACKGROUND = "CategoryRecipes";
     public static final String STR_RECIPE = "Recipes";
+    public static final String STR_USEFUL = "Useful";
     public static String CATEGORY_SELECTED;
     public static String CATEGORY_ID_SELECTED;
     public static String ADMIN_LOGIN = "Admin";
--- app/src/main/java/com/nonexistentware/recipeappv2/Fragment/TipsListFragments.java
+++ app/src/main/java/com/nonexistentware/recipeappv2/Fragment/TipsListFragments.java
@@ -4,18 +4,95 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nonexistentware.recipeappv2.Common.Common;
+import com.nonexistentware.recipeappv2.Interface.ItemClickListener;
+import com.nonexistentware.recipeappv2.Model.UsefulItem;
 import com.nonexistentware.recipeappv2.R;
+import com.nonexistentware.recipeappv2.ViewHolder.UsefulListViewHolder;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 public class TipsListFragments extends Fragment {
+
+    FirebaseDatabase database;
+    DatabaseReference tipItemBase;
+
+    RecyclerView recyclerView;
+
+    FirebaseRecyclerOptions<UsefulItem> options;
+    FirebaseRecyclerAdapter<UsefulItem, UsefulListViewHolder> adapter;
 
     private static TipsListFragments INSTANCE = null;
 
     public TipsListFragments() {
+        database = FirebaseDatabase.getInstance();
+        tipItemBase = database.getReference(Common.STR_USEFUL);
 
+        options = new FirebaseRecyclerOptions.Builder<UsefulItem>()
+                .setQuery(tipItemBase, UsefulItem.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<UsefulItem, UsefulListViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final UsefulListViewHolder holder, int position, @NonNull final UsefulItem model) {
+                Picasso.with(getActivity())
+                        .load(model.getuImageLink())
+                        .networkPolicy(NetworkPolicy.OFFLINE)
+                        .into(holder.tipItemImage, new Callback() {
+                            @Override
+                            public void onSuccess() {
+
+                            }
+
+                            @Override
+                            public void onError() {
+                                Picasso.with(getActivity())
+                                        .load(model.getuImageLink())
+                                        .error(R.drawable.ic_error_outline_white_24dp)
+                                        .into(holder.tipItemImage, new Callback() {
+                                            @Override
+                                            public void onSuccess() {
+
+                                            }
+
+                                            @Override
+                                            public void onError() {
+                                                Log.e("ERROR", "Couldn't fetch image");
+                                            }
+                                        });
+                            }
+                        });
+
+                holder.tipItemName.setText(model.getuName());
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public UsefulListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.layout_useful_item, parent, false);
+                return new UsefulListViewHolder(itemView);
+            }
+        };
     }
 
     public static TipsListFragments getInstance() {
@@ -32,6 +109,39 @@ public class TipsListFragments extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_tips_fragments, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_tips_fragments, container, false);
+       recyclerView = view.findViewById(R.id.recycler_list_useful);
+       recyclerView.setHasFixedSize(true);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        setUsefulCategory();
+        return view;
+    }
+
+    private void setUsefulCategory() {
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (adapter != null)
+            adapter.startListening();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (adapter != null)
+            adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        if (adapter != null)
+            adapter.stopListening();
+        super.onStop();
     }
 }
